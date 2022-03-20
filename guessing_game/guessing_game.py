ce64937b580e00e940a8156dbe51f2c3e9c1ff21
Sidakveer <sidakveersingh8@gmail.com>
2022-03-20 15:33:56 -0400

    finish code for game

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -1,34 +1,57 @@
 import random
 import re
 
+def play_game():
+    answer = random.randint(1, 101)
 
-answer = random.randint(1, 101)
+    print("Welcome to the guessing game!")
+    print("I am thinking of a number between 1 and 100")
 
-print("Welcome to the guessing game!")
-print("I am thinking of a number between 1 and 100")
+    difficulty = input("Choose difficulty. Type easy or hard")
 
-difficulty = input("Choose difficulty. Type easy or hard").casefold
+    guess_correct = False
+    if difficulty == "easy".casefold():
+        attempts = 10
 
-guess_correct = False
-if difficulty == "easy":
-    attempts = 10
+        for x in range(attempts):
+            print(f"You have {10 - x} attempts remaining")
+            guess = int(input("Make a guess: "))
+            if guess == answer:
+                print(f"You got it. The answer is {answer}")
+                guess_correct = True
+                break
+            elif guess < answer:
+                print("Too low.")
+            else:
+                print("Too high.")
 
-    for x in range(attempts):
-        print(f"You have {attempts} attempts remaining")
-        guess = int(input("Make a guess: "))
-        if guess == answer:
-            print(f"You got it. The answer is {answer}")
-            guess_correct = True
-            break
-        elif guess < answer:
-            print("Too low.")
-        else:
-            print("Too high.")
-            
-    if not guess_correct:
-        print(f"Sorry you lost. The answer was {answer}")
+        if not guess_correct:
+            print(f"Sorry you lost. The answer was {answer}")
 
 
 
-else:
-    attempts = 5
+    else:
+        attempts = 5
+
+        for x in range(attempts):
+            print(f"You have {5 - x} attempts remaining")
+            guess = int(input("Make a guess: "))
+            if guess == answer:
+                print(f"You got it. The answer is {answer}")
+                guess_correct = True
+                break
+            elif guess < answer:
+                print("Too low.")
+            else:
+                print("Too high.")
+
+        if not guess_correct:
+            print(f"Sorry you lost. The answer was {answer}")
+
+
+while True:
+    play = input("Would you like to play the guessing game y/n ? ")
+    if play == "y":
+        play_game()
+    else:
+        break
